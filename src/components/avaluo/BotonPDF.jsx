a4f194915c2a7dce9399e8ed6b83788d7a88181a
Andrés Quiceno <wquiceno10@gmail.com>
2025-12-08 15:30:09 -0500

    fix(PDF): Increase Precio m max-width to 150px

--- src/components/avaluo/BotonPDF.jsx
+++ src/components/avaluo/BotonPDF.jsx
@@ -854,7 +854,7 @@ ail-row" style="align-items: center;">
                   <th style="text-align:center; vertical-align:middle; max-width: 50px;">Hab/<br>Baños</th>
                   <th style="text-align:center; vertical-align:middle; min-width: 135px;">Precio Publicado</th>
                   <th style="text-align:center; vertical-align:middle; min-width: 135px;">Precio de Venta</th>
-                  <th style="text-align:center; vertical-align:middle;max-width: 100px">Precio m²</th>
+                  <th style="text-align:center; vertical-align:middle;max-width: 150px">Precio m²</th>
                 </tr>
               </thead>
 
